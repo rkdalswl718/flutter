6175e2a66e500c8594ac199487dec708c530ce8c
yunse0708 <yunse070708@gmail.com>
2024-06-13 14:44:48 +0900

    shopping

---
+++ lib/components/product_selector_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/constants.dart';
+
+class ProductSelectorButton extends StatelessWidget {
+  final int id;
+  final int selectedIconNum;
+  final IconData mIcon;
+  final Function changeIcon;
+
+  const ProductSelectorButton(
+      {Key? key,
+      required this.id,
+      required this.selectedIconNum,
+      required this.mIcon,
+      required this.changeIcon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: id == selectedIconNum ? kAccentColor : kSecondaryColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: IconButton(
+        icon: Icon(mIcon, color: Colors.black),
+        onPressed: () {
+          changeIcon(id);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/components/shoppingcart_detail.dart';
+import 'package:flutter_shoppingcart/components/shoppingcart_header.dart';
+import 'package:flutter_shoppingcart/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: Column(
+        children: [
+          WeatherHeader(),
+          Expanded(child: ShoppingCartDetail()),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+        SizedBox(width: 16),
+      ],
+      elevation: 0.0,
+    );
+  }
+}
